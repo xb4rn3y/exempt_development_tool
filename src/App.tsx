035de1258776172b73246e34519480116df84c34
anandannam3-afk <anand.annam3@gmail.com>
2025-09-22 12:21:31 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
-import Index from "./pages/Index";
+import Index from "./pages/index";
 import ZoneCheck from "./pages/ZoneCheck";
 import Properties from "./pages/Properties";
 import StructureInput from "./pages/StructureInput";
